185daa0e4f6cb1eed86880ea55849def1be7101a
janyroo <214778990@qq.com>
2019-03-10 19:21:31 +0800

    heroku提交

--- herojany/settings.py
+++ herojany/settings.py
@@ -122,9 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
+'''STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
-)
+)'''
 # Django用户上传的都叫media文件
 MEDIA_URL = "/media/"
 # media配置，用户上传的文件都默认放在这个文件夹下
