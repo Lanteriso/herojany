ac67fed7c927f9649f96ea6b34649ec2cb92c844
janyroo <214778990@qq.com>
2019-03-11 20:51:54 +0800

    heroku提交

---
+++ todolist/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path,include
+from . import views
+
+app_name='todolist'
+urlpatterns = [
+
+    path('home/', views.home,name='主页'),
+    path('about/', views.about,name='关于'),
+    path('edit/<每一件事_id>', views.edit, name='编辑'),
+    path('del/<每一件事_id>', views.delete, name='删除'),
+    path('cross/<每一件事_id>', views.cross,name='划掉'),
+    path('timeing/<每一件事_id>', views.timeing,name='计时'),
+]
